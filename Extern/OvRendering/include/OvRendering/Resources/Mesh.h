c4f9a7402eaaf0c52331097b3b3e3bca444de850
qwerqwerqwerqwer qwerqwerqwerqwe <271812697@qq.com>
2025-11-07 17:50:02 +0800

    update

--- Extern/OvRendering/include/OvRendering/Resources/Mesh.h
+++ Extern/OvRendering/include/OvRendering/Resources/Mesh.h
@@ -56,7 +56,8 @@ namespace OvRendering::Resources
 		/**
 		* Returns the material index of the mesh
 		*/
-		uint32_t GetMaterialIndex() const;
+		std::vector<uint32_t> GetMaterialIndex() const;
+		void AddMaterial(int materialIndex);
 
 	private:
 		void Upload(std::span<const Geometry::Vertex> p_vertices, std::span<const uint32_t> p_indices);
@@ -65,8 +66,8 @@ namespace OvRendering::Resources
 	private:
 		const uint32_t m_vertexCount;
 		const uint32_t m_indicesCount;
-		const uint32_t m_materialIndex;
-
+		std::vector<uint32_t>m_materialIndex;
+		
 		HAL::VertexArray m_vertexArray;
 		HAL::VertexBuffer m_vertexBuffer;
 		HAL::IndexBuffer m_indexBuffer;
