bef7932eeeea9a1a41a85d54dbd7bef546c1941a
buranya <38773493+271812697@users.noreply.github.com>
2026-07-18 08:06:04 +0800

    Update FilletTask.cpp

--- Moon/editor/UI/TaskPanel/FilletTask.cpp
+++ Moon/editor/UI/TaskPanel/FilletTask.cpp
@@ -44,7 +44,7 @@ namespace MOON {
                 ViewTool::getSelectedBasedFeature(baseFeature, subValues);
                 if (baseFeature) {
                     isCreatedFeature = true;
-                    feature = new FilletFeature("Thickness");
+                    feature = new FilletFeature("Fillet");
                     feature->setBaseFeature(baseFeature);
                     feature->setSubValues(subValues);
                     self->setFeature(feature);
@@ -91,98 +91,6 @@ namespace MOON {
                 axisBehaviour1->AddObserver(AxisTranslationEvent::LengthChange, self, &FilletTask::onWidgetLengthInvoke1);
                 axisBehaviour2->AddObserver(AxisTranslationEvent::LengthChange, self, &FilletTask::onWidgetLengthInvoke2);
             }
-
-            //ViewTool::getSelectedTopoShape(shapes);
-            //if (shapes.size() > 0) {
-            //    if (shapes[1].getShape().ShapeType() != TopAbs_ShapeEnum::TopAbs_EDGE) {
-            //        CORE_ERROR("is not a Edge to fillet");
-            //        return;
-            //    }
-            //    auto len = shapes[0].getBoundBoxOptimal().CalcDiagonalLength()*0.01;;
-            //    feature->radius = shapes[0].getBoundBoxOptimal().CalcDiagonalLength() * 0.03;
-
-            //    axisBehaviour1 = new AxisTranslationWidget("fillet");
-            //    axisBehaviour2 = new AxisTranslationWidget("fillet");
-            //    axisBehaviour1->setUpScale(len);
-            //    axisBehaviour2->setUpScale(len);
-            //    // 1. 获取边
-            //    TopoDS_Edge edge = TopoDS::Edge(shapes[1].getShape());
-
-            //    // 2. 获取边的中点坐标和切向量
-            //    double first, last;
-            //    Handle(Geom_Curve) curve = BRep_Tool::Curve(edge, first, last);
-            //    double midParam = (first + last) * 0.5;
-            //    gp_Pnt midPoint;
-            //    gp_Vec tangent;
-            //    curve->D1(midParam, midPoint, tangent);
-            //    tangent.Normalize();
-
-            //    // 3. 获取两个面
-            //    auto faces = shapes[0].findAncestorsShapes(shapes[1].getShape(), TopAbs_FACE);
-            //    auto solids = shapes[0].findAncestorsShapes(shapes[1].getShape(), TopAbs_SOLID);
-            //    if (faces.size() != 2) {
-            //        CORE_ERROR("Edge must be shared by exactly two faces");
-            //        return;
-            //    }
-            //    TopoDS_Face face1 = TopoDS::Face(faces[0]);
-            //    TopoDS_Face face2 = TopoDS::Face(faces[1]);
-            //    TopoDS_Solid solid = TopoDS::Solid(solids[0]);
-
-            //    // 4. 计算某个面上、过边上一点、位于面内且垂直于边的方向
-            //    auto getInPlanePerpDir = [&](const TopoDS_Solid& solid,const TopoDS_Face& face, const gp_Pnt& point, const gp_Vec& tangent) -> gp_Vec {
-            //        BRepAdaptor_Surface surf(face);
-
-            //        gp_Vec normal;
-            //        if (surf.GetType() == GeomAbs_Plane) {
-            //            // 平面：直接用平面法向
-            //            normal = surf.Plane().Axis().Direction();
-            //        }
-            //        else {
-            //            // 曲面：将点投影到曲面，获取 uv 参数后计算法向
-            //            Handle(Geom_Surface) surface = BRep_Tool::Surface(face);
-            //            GeomAPI_ProjectPointOnSurf projector(point, surface);
-            //            if (projector.NbPoints() == 0) {
-            //                CORE_ERROR("Failed to project point onto surface");
-            //                return gp_Vec(0, 0, 1); // 默认方向
-            //            }
-            //            double u, v;
-            //            projector.Parameters(1, u, v);
-            //            gp_Pnt P;
-            //            gp_Vec dU, dV;
-            //            surf.D1(u, v, P, dU, dV);
-            //            normal = dU.Crossed(dV);
-            //        }
-            //        normal.Normalize();
-            //        // 面内垂直于边的方向 = 切向量 × 法向量
-            //        gp_Vec dir = tangent.Crossed(normal);
-            //        dir.Normalize();
-            //        // 沿 dir 方向偏移一点
-            //        gp_Pnt testPoint = point.XYZ() + dir.XYZ() * 0.001;
-            //        // 判断 testPoint 是否在实体内部
-            //        BRepClass3d_SolidClassifier classifier(solid);
-            //        classifier.Perform(testPoint, Precision::Confusion());
-            //        if (classifier.State() == TopAbs_OUT) {
-            //            // 如果在外部，反转方向
-            //            dir.Reverse();
-            //        }
-            //        return dir;
-            //        };
-
-            //    gp_Vec dir1 = getInPlanePerpDir(solid,face1, midPoint, tangent);
-            //    gp_Vec dir2 = getInPlanePerpDir(solid,face2, midPoint, tangent);
-
-
-            //    //axisBehaviour1->setImmediateInvoke(false);
-            //    axisBehaviour1->setLength(feature->radius);
-            //    axisBehaviour1->setUpOrigin(midPoint.X(), midPoint.Y(), midPoint.Z());
-            //    axisBehaviour1->setUpDir(dir1.X(), dir1.Y(), dir1.Z());
-            //    //axisBehaviour2->setImmediateInvoke(false);
-            //    axisBehaviour2->setLength(feature->radius);
-            //    axisBehaviour2->setUpOrigin(midPoint.X(), midPoint.Y(), midPoint.Z());
-            //    axisBehaviour2->setUpDir(dir2.X(), dir2.Y(), dir2.Z());
-            //    axisBehaviour1->AddObserver(AxisTranslationEvent::LengthChange, self, &FilletTask::onWidgetLengthInvoke1);
-            //    axisBehaviour2->AddObserver(AxisTranslationEvent::LengthChange, self, &FilletTask::onWidgetLengthInvoke2);
-            //}
         }
         ~Internal() {
             if (axisBehaviour1) {
